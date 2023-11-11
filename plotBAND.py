6d82a6d078c17cff4107e98505cc565f93de19ba
Keerthan Rao <keerthanr1@gmail.com>
2023-11-11 14:04:03 -0500

    updated plotBAND.py and plotDOS.py

--- plotBAND.py
+++ plotBAND.py
@@ -1,31 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_1 = np.loadtxt('orthoBAND.dat')
-plt.figure(1)
-print(data_1)
-plt.plot(data_1[:,0],data_1[:,1],c = 'royalblue')
-plt.xlim(0,3)
-plt.ylim(-5,5)
-plt.xlabel('Wave Vector',fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.ylabel('Energy (eV)',fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.title('Orthorhombic',fontweight='bold')
-#plt.xticks([])
-plt.savefig("orthoBAND.png",dpi=300)
-plt.show()
+def plot_band(data, xlim, ylim, title, filename):
+    """
+    Plot band data and save the plot.
 
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Array containing band data with two columns (wave vector and energy).
+    xlim : tuple
+        Tuple specifying the x-axis limits.
+    ylim : tuple
+        Tuple specifying the y-axis limits.
+    title : str
+        Title for the plot.
+    filename : str
+        Filename to save the plot.
 
-data_2 = np.loadtxt('cubicBAND.dat')
-plt.figure(2)
-#print(data_2)
-plt.plot(data_2[:,0],data_2[:,1],c = 'royalblue')
-plt.xlim(0,2.5)
-plt.ylim(-5,5)
-plt.xlabel('Wave Vector',fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.ylabel('Energy (eV)',fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-#plt.axvline(x=0,label='G',c = 'black')
-#plt.axvline(x=0.5,label='X',c = 'black')
-plt.title('Cubic',fontweight='bold')
-#plt.xticks([])
-plt.savefig("cubicBAND.png",dpi=300)
-plt.show()
+    Returns
+    -------
+    None
+    """
+    plt.plot(data[:, 0], data[:, 1], c='royalblue')
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.xlabel('Wave Vector', fontstyle='normal', c='black', fontfamily='sans-serif', fontweight='bold')
+    plt.ylabel('Energy (eV)', fontstyle='normal', c='black', fontfamily='sans-serif', fontweight='bold')
+    plt.title(title, fontweight='bold')
+    plt.savefig(filename, dpi=300)
+    plt.show()
+
+def main():
+    """
+    Main function to load band data and create plots for orthorhombic and cubic structures.
+
+    Returns
+    -------
+    None
+    """
+    data_1 = np.loadtxt('orthoBAND.dat')
+    plot_band(data_1, (0, 3), (-5, 5), 'Orthorhombic', 'orthoBAND.png')
+
+    data_2 = np.loadtxt('cubicBAND.dat')
+    plot_band(data_2, (0, 2.5), (-5, 5), 'Cubic', 'cubicBAND.png')
+
+if __name__ == "__main__":
+    main()
--- plotDOS.py
+++ plotDOS.py
@@ -1,36 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
-'''
-data_1 = np.loadtxt('BNH3.dat')
-plt.figure(1)
-plt.plot(data_1[7930:10000, 0], data_1[7930:10000, 1], label='Sn(p)', c='grey') # Tin (p)
-plt.plot(data_1[12925:15000, 0], data_1[12925:15000, 1], label='I(p)', c='violet') # Iodine (p)
-plt.plot(data_1[2940:5000, 0], data_1[2940:5000, 1], c='forestgreen') # Total DOS
-plt.legend()
-plt.xlabel("Energy (eV)",fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.ylabel("Density (states/eV)",fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.title("Anilinium",fontweight="bold")
 
-plt.ylim(0)
-plt.xlim(-4,5) # Sets a limit
-plt.yticks([]) # Removes y axis numbers
-plt.xticks(np.arange(-4,5,1))
-#plt.savefig('Anilinium_Sn_I_states.png',dpi = 300)
-'''
-plt.figure(2)
-data_2 = np.loadtxt('BF5NH3_C_N_F.dat')
-plt.plot(data_2[7930:10000, 0], data_2[7930:10000, 1], label='N(p)', c='skyblue') # C(p) DOS
-plt.plot(data_2[12925:15000, 0], data_2[12925:15000, 1], label='C(p)', c='peru') # N(p) DOS
-plt.plot(data_2[17925:20000, 0], data_2[17925:20000, 1], label='F(p)', c='royalblue') # F(p) DOS
-plt.plot(data_2[2940:5000, 0], data_2[2940:5000, 1], c='forestgreen') # Total DOS
-plt.legend()
-plt.xlabel("Energy (eV)",fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.ylabel("Density (states/eV)",fontstyle='normal', c='black',fontfamily='sans-serif',fontweight='bold')
-plt.title("2,3,4,5,6-FAn",fontweight="bold")
+def plot_dos(data, energy_range, labels, colors, title, filename):
+    """
+    Plot density of states (DOS) data and save the plot.
 
-plt.ylim(0)
-plt.xlim(-4,5) # Sets a limit
-plt.yticks([]) # Removes y axis numbers
-plt.xticks(np.arange(-4,5,1))
-plt.savefig('BF5NH3_C_N_F_states.png',dpi=300)
-plt.show()
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Array containing DOS data with two columns (energy and density).
+    energy_range : tuple
+        Tuple specifying the energy range for the plot.
+    labels : list
+        List of labels for each DOS curve.
+    colors : list
+        List of colors for each DOS curve.
+    title : str
+        Title for the plot.
+    filename : str
+        Filename to save the plot.
+
+    Returns
+    -------
+    None
+    """
+    plt.plot(data[energy_range[0]:energy_range[1], 0], data[energy_range[0]:energy_range[1], 1], label=labels[0], c=colors[0])
+    plt.plot(data[energy_range[2]:energy_range[3], 0], data[energy_range[2]:energy_range[3], 1], label=labels[1], c=colors[1])
+    plt.plot(data[energy_range[4]:energy_range[5], 0], data[energy_range[4]:energy_range[5], 1], label=labels[2], c=colors[2])
+    plt.plot(data[energy_range[6]:energy_range[7], 0], data[energy_range[6]:energy_range[7], 1], c=colors[3])
+    plt.legend()
+    plt.xlabel("Energy (eV)", fontstyle='normal', c='black', fontfamily='sans-serif', fontweight='bold')
+    plt.ylabel("Density (states/eV)", fontstyle='normal', c='black', fontfamily='sans-serif', fontweight='bold')
+    plt.title(title, fontweight="bold")
+    plt.ylim(0)
+    plt.xlim(energy_range[8], energy_range[9])  # Sets a limit
+    plt.yticks([])  # Removes y-axis numbers
+    plt.xticks(np.arange(energy_range[8], energy_range[9], 1))
+    plt.savefig(filename, dpi=300)
+    plt.show()
+
+def main():
+    """
+    Main function to load DOS data and create plots for Anilinium and 2,3,4,5,6-FAn.
+
+    Returns
+    -------
+    None
+    """
+    plt.figure(1)
+    data_1 = np.loadtxt('BNH3.dat')
+    plot_dos(data_1, (7930, 10000, 12925, 15000, 2940, 5000, 0, 5, -4, 5), ['Sn(p)', 'I(p)', 'Total DOS'], ['grey', 'violet', 'forestgreen'], 'Anilinium', 'Anilinium_Sn_I_states.png')
+
+    plt.figure(2)
+    data_2 = np.loadtxt('BF5NH3_C_N_F.dat')
+    plot_dos(data_2, (7930, 10000, 12925, 15000, 17925, 20000, 2940, 5000, -4, 5), ['N(p)', 'C(p)', 'F(p)', 'Total DOS'], ['skyblue', 'peru', 'royalblue', 'forestgreen'], '2,3,4,5,6-FAn', 'BF5NH3_C_N_F_states.png')
+
+if __name__ == "__main__":
+    main()
